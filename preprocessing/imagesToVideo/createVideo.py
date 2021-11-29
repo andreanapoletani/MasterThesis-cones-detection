d0a28070ab60014d12e0973309a38863e129934e
Andrea Napoletani <andrea.napoletani@gmail.com>
2021-11-29 17:57:20 +0100

    Add create video script

---
+++ preprocessing/imagesToVideo/createVideo.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import os
+ 
+source_path = '../../../../Desktop/giro/'
+img_array = []
+for filename in sorted(os.listdir(source_path)):
+    print(filename)
+    img = cv2.imread(source_path + filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+ 
+ 
+out = cv2.VideoWriter('../../../../Desktop/lap15fps.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
+ 
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
