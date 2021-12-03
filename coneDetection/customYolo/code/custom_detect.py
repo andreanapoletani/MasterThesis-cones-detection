Inference on ROI but wrong BB scaling on result

coneDetection/customYolo/code/custom_detect.py
@@ -91,7 +91,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
     # Run inference
     model.warmup(imgsz=(1, 3, *imgsz), half=half)  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
-    for path, im, im0s, vid_cap, s in dataset:
+    for path, im, im0s, vid_cap, s, original_img in dataset:
         t1 = time_sync()
         im = torch.from_numpy(im).to(device)
         im = im.half() if half else im.float()  # uint8 to fp16/32
@@ -106,6 +106,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         pred = model(im, augment=augment, visualize=visualize)
         t3 = time_sync()
         dt[1] += t3 - t2
+
 
         # NMS
         pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
@@ -129,10 +130,11 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
             s += '%gx%g ' % im.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
-            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            annotator = Annotator(original_img, line_width=line_thickness, example=str(names))
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+                det[:, :4] = scale_coords(im0.shape[2:], det[:, :4], original_img.shape).round()
 
                 # Print results
                 for c in det[:, -1].unique():
@@ -156,6 +158,10 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
             # Print time (inference-only)
             LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
+
+            # rectangular ROI -----------------> TEST
+            #cv2.rectangle(im0, (220, 290), (420, 190), (255,0,0), 2)
+            #cv2.rectangle(im0, (340, 360), (940, 160), (255,0,0), 2)
 
             # Stream results
             im0 = annotator.result()

coneDetection/customYolo/code/utils/datasets.py
@@ -227,13 +227,18 @@ class LoadImages:
             img0 = cv2.imread(path)  # BGR
 
             # custom black mask applied on img ---------------------------------
-            mask = cv2.imread('blackmask.png',0)
-            img0 = cv2.bitwise_and(img0,img0,mask = mask)
-            print(str(self.count)+'.jpg')
-            cv2.imwrite(str(self.count)+'.jpg', img0)
+            img0 = cv2.bitwise_and(img0,img0,mask = self.mask)
             # --------------------------------------------
             assert img0 is not None, f'Image Not Found {path}'
             s = f'image {self.count}/{self.nf} {path}: '
+
+            # -----------------------> TEST
+            # test inference on a ROI
+            print(img0.shape)
+            #img0 = img0[416:544, 576:704]
+            #img0 = img0[154:532, 160:538, :]
+            original_img = img0
+            img0 = img0[128:430, 225:1098]
 
         # Padded resize
         img = letterbox(img0, self.img_size, stride=self.stride, auto=self.auto)[0]
@@ -242,7 +247,7 @@ class LoadImages:
         img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
         img = np.ascontiguousarray(img)
 
-        return path, img, img0, self.cap, s
+        return path, img, img0, self.cap, s, original_img
 
     def new_video(self, path):
         self.frame = 0